e81894b2c1ee0a28e069840a3e7747876f9cd099
Terry Boufis <terryboufis@gmail.com>
2022-12-10 14:43:38 -0600

    got the notes to work

--- db/store.js
+++ db/store.js
@@ -1,5 +1,6 @@
 const util = require("util");
 const fs = require("fs");
+const {v4:getId} = require("uuid");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -8,15 +9,40 @@ class Store {
   read() {
     return readFileAsync("db/db.json", "utf8");
   }
-  write() {
+  write(note) {
     return writeFileAsync("db/db.json", JSON.stringify(note));
   }
 
   getNotes() {
     return this.read().then((notes) => {
-      return JSON.parse(notes);
-    });
+      return JSON.parse(notes) || [];
+    });  
   }
+
+  addNote(note) {
+    if (note.title === undefined || note.text === undefined) {
+      throw new Error()
+    }
+    return this.getNotes().then(notes => {
+      note.id = getId()
+      // const notesArray = notes.push(note)
+      return [...notes, note]
+    
+    }).then(updatedNotes => {
+      return this.write(updatedNotes)
+  
+    }).then(() => {
+      return note
+    })
+  }
+
+
+
+
+
+  
 }
+
+
 
 module.exports = new Store();
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,25 @@
 const router = require("express").Router();
 const store = require("../db/store");
+
 router.get("/notes", (req, res) => {
   store
     .getNotes()
     .then((notes) => res.json(notes))
     .catch((err) => res.status(500).json(err));
 });
+
+router.post("/notes", (req, res) => {
+  store
+    .addNote(req.body)
+    .then((updatedNotes) => res.json(updatedNotes))
+    .catch((err) => res.status(500).json(err));
+});
+
+router.delete("/notes", (req, res) => {
+  store
+    .deleteNotes()
+    .then((deleteNotes) => res.json(deleteNotes))
+    .catch((err) => res.status(500).json(err));
+})
 
 module.exports = router;
